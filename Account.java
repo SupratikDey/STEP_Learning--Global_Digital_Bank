95f4eda892d0dcf15bbd035075d596697a92c718
SupratikDey <spratik.dey@gmail.com>
2026-08-29 16:52:25 +0530

    Activity 3 - Add validation, business rules, and security features to the Account entity class using boolean returns.

--- Account.java
+++ Account.java
@@ -5,6 +5,7 @@ class Account{
     private double balance;
     private String accountType;
     private String status;
+    private Integer pin;
 
     public Account(int accountNumber,String name,int age,int initialBalance,String accountType){
         this.accountNumber = accountNumber;
@@ -24,19 +25,28 @@ class Account{
     }
 
     public boolean deposit(double amount){
-        if(amount<=0){
+        if(amount<=0 || this.status.equals("Inactive")){
             return false;
         }
         this.balance += amount;
         return true;
     }
 
-    public boolean withdraw(double amount){
-        if(amount<=0 || this.balance<amount ){
+    public boolean withdraw(double amount , int pin){
+        if(amount<=0 || this.balance<amount || this.status.equals("Inactive")){
             return false;
         }
-        this.balance -= amount;
-        return true;
+        if(verifyPin(pin)){
+            double remain_value = this.balance-amount;
+            double min_value = this.accountType.equals("Savings")?500:1000;
+            if(remain_value<min_value){
+                return false;
+            }
+            this.balance = remain_value;
+            return true;            
+        }
+        return false;    
+          
     }
 
     public int getAccountNumber(){
@@ -71,4 +81,43 @@ class Account{
         this.age = age;
     }
 
+    public Boolean closeAccount(){
+        if(this.status.equals("Inactive")){
+            return false;
+        }
+        this.status = "Inactive";
+        return true;
+    }
+
+    public Boolean reopenAccount(){
+        if(this.status.equals("Active")){
+            return false;
+        }
+        this.status = "Active";
+        return true;
+    }
+
+    public boolean setPin(Integer pin){
+        if(pin == null || (pin<=9999 && pin>=1000)){
+            this.pin = pin;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasPin(){
+        if(this.pin==null){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean verifyPin(int pin){
+        if(hasPin()){
+            if(pin==this.pin){
+                return true;
+            }
+        }
+        return false;
+    }
 }
